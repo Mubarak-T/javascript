d86b14fce1af8f8fbee77b2db41cb25a61fa0f7f
Mubarak-T <mubarakthayyil4@gmail.com>
2024-07-17 17:14:43 +0530

    index file created

---
+++ script.js
@@ -0,0 +1,21 @@
+// Data Types
+// 1. Primitive
+// 2. Reference
+
+
+
+let num = 10
+let num2 = num
+num = num + 1
+
+// 0,1,2,3
+
+let students = ["S1","S2","S3",30,]
+let students2 = students
+
+students[0] = "S30"
+
+console.log(num)
+console.log(num2)
+console.log(students)
+console.log(students2)
